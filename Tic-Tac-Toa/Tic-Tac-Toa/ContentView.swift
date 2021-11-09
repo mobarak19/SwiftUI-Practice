3e8b79bbfc641f362682dd849370411712892de4
mobarak <mobarakhossen2012@gmail.com>
2021-11-09 10:35:54 +0600

    tic tac toa

---
+++ Tic-Tac-Toa/Tic-Tac-Toa/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toa
+//
+//  Created by Mobarak on 11/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let columns : [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
+    var body: some View {
+        
+        GeometryReader{ geometry in
+            VStack{
+                Spacer()
+                LazyVGrid(columns: columns,spacing:5){
+                    ForEach(0..<9){ i in
+                        ZStack{
+                            Circle()
+                                .foregroundColor(.red).opacity(0.5)
+                                .frame(width: geometry.size.width/3-15, height: geometry.size.width/3-15)
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
+
+                        }.onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .human: .computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
+                    }
+                }
+                Spacer()
+            }.padding()
+            
+        }
+
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player:Player
+    let boardIndex:Int
+    var indicator: String{
+        return player == .human ? "xmark":"circle"
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic-Tac-Toa/Tic-Tac-Toa/Tic_Tac_ToaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToaApp.swift
+//  Tic-Tac-Toa
+//
+//  Created by Mobarak on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
